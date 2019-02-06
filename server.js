e8f5a4e40fa126ec98382b680a7de5a7e09b387b
Steven Rothenburger <s.rothenburger1@gmail.com>
2019-02-05 18:10:01 -0700

    formatted code to look nicer

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-
 const express = require("express");
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
 const app = express();
 
@@ -12,91 +11,87 @@ app.use(cors());
 const database = {
   users: [
     {
-      id: '123',
-      name:'John',
-      password:'cookies',
-      email:'john@gmail.com',
+      id: "123",
+      name: "John",
+      password: "cookies",
+      email: "john@gmail.com",
       entries: 0,
       joined: new Date()
     },
     {
-      id: '124',
-      name:'Sally',
-      password:'bananas',
-      email:'sally@gmail.com',
+      id: "124",
+      name: "Sally",
+      password: "bananas",
+      email: "sally@gmail.com",
       entries: 0,
       joined: new Date()
     }
   ]
-}
-
+};
 
 app.get("/", (req, res) => {
   res.send(database.users);
 });
 
-
-
-app.post('/signin',(req, res)=>{
-// Load hash from your password DB.
-bcrypt.compare("bacon", 'hash', function(err, res) {
-  // res == true
-});
-bcrypt.compare("veggies", 'hash', function(err, res) {
-  // res = false
-});
-  if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-  res.json('success');
-}else{
-  res.status(400).json('error logging in');
-}
-  res.json('signin')
-})
-
-app.post('/register', (req,res)=>{
-  const { email, name, password } = req.body;
-  bcrypt.hash(password, null, null, function(err, hash) {
-    // Store hash in your password DB.
+app.post("/signin", (req, res) => {
+  // Load hash from your password DB.
+  bcrypt.compare("bacon", "hash", function(err, res) {
+    // res == true
   });
-  database.users.push({
-      id: '125',
-      name:name,
-      email:email,
-      password:password,
-      entries: 0,
-      joined: new Date()
-  })
-  res.json(database.users[database.users.length-1])
-})
-
-app.get('/profile/:id',(req,res)=>{
-const { id } = req.params;
-let found =false;
-database.users.forEach(user =>{
-  if(user.id === id){
-    found = true;
-    return res.json(user)
+  bcrypt.compare("veggies", "hash", function(err, res) {
+    // res = false
+  });
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
   }
- })
-if(!found){
-  res.status(400).json('not found')
-}
-})
+  res.json("signin");
+});
 
-app.post('/image',(req,res)=>{
+app.post("/register", (req, res) => {
+  const { email, name } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.post("/image", (req, res) => {
   const { id } = req.body;
-  let found =false;
-  database.users.forEach(user =>{
-    if(user.id === id){
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
       found = true;
       user.entries++;
-      return res.json(user.entries)
+      return res.json(user.entries);
     }
-  })
-  if(!found){
-    res.status(400).json('not found')
-  }    
-})
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
 
 // bcrypt.hash("bacon", null, null, function(err, hash) {
 //   // Store hash in your password DB.
@@ -109,8 +104,6 @@ app.post('/image',(req,res)=>{
 // bcrypt.compare("veggies", hash, function(err, res) {
 //   // res = false
 // });
-
-
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
