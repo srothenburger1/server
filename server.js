4fec331edc3f8c3f984cf9cce28f112538e34bb6
Steven Rothenburger <s.rothenburger1@gmail.com>
2019-05-31 10:50:17 -0600

    fixed database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const knex = require('knex')({
 	client: 'pg',
 	connection: {
-		host: process.env.DATABASE_URL,
+		connectionString: process.env.DATABASE_URL,
 		ssl:true,
 	},
 });
