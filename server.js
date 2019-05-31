0019d5bcc8911bd9bfee9cfdc2ad8aca1927587f
Steven Rothenburger <s.rothenburger1@gmail.com>
2019-05-31 10:46:58 -0600

    fixed database

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const app = express();
 const knex = require('knex')({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
-		user: 'postgres',
-		password: 'password',
-		database: 'smart-brain',
+		host: process.env.DATABASE_URL,
+		ssl:true,
 	},
 });
 
